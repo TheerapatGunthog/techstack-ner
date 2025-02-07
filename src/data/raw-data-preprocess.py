8fefc7b811f64687e6c10162f50ad094a2954bdc
WhileBELL <bellbelieve.work@gmail.com>
2025-02-07 17:37:14 +0700

    update .gitignore, update code format and add data quality check in raw-data-preprocess

--- src/data/raw-data-preprocess.py
+++ src/data/raw-data-preprocess.py
@@ -1,5 +1,4 @@
 import sys
-import yaml
 import re
 from pathlib import Path
 import pandas as pd
@@ -8,76 +7,16 @@ import spacy
 
 sys.path.append(str(Path(__file__).resolve().parent.parent.parent))
 
-from data.keywords import KEYWORDS_DATA_PATH
 from data.raw import RAW_DATA_PATH
 from data.interim import INTERIM_DATA_PATH
 
 tqdm.pandas()
 
 
-class MergeDataset:
-    def __init__(self, raw_data_path):
-        self.data_directory = Path(raw_data_path) / "csv1"
-
-    def merge_csv_files(self, output_file):
-        all_files = list(self.data_directory.glob("*.csv"))
-        df_list = [pd.read_csv(file) for file in all_files]
-        merged_df = pd.concat(df_list, ignore_index=True)
-        merged_df.to_csv(output_file, index=False)
-
-
-def load_keywords(yaml_file):
-    with open(yaml_file, "r", encoding="utf-8") as file:
-        data = yaml.safe_load(file)
-    return data.get("keywords", [])
-
-
-def escape_regex(keyword):
-    return re.escape(keyword).replace(r"\ ", " ")
-
-
-class DataSelection:
-    def __init__(self, raw_data_path, keywords_data_path):
-        self.dataset = Path(raw_data_path) / "merged.csv"
-        self.df = pd.read_csv(self.dataset)
-        self.keywords = load_keywords(
-            Path(keywords_data_path) / "jobs-title-keyword-copy.yaml"
-        )
-
-    def clean_unused_columns(self):
-        self.df = self.df[["Topic", "Qualification"]]
-        self.df.dropna(subset=["Topic"], inplace=True)
-
-        # Remove rows that contain Thai characters
-        self.df = self.df[~self.df["Topic"].str.contains(r"[\u0E00-\u0E7F]", na=False)]
-
-    def jobs_title_filter(self):
-        self.df["Topic"] = self.df["Topic"].str.lower().fillna("")
-
-        escaped_keywords = [escape_regex(keyword) for keyword in self.keywords]
-        pattern = r"\b(" + "|".join(escaped_keywords) + r")\b"
-
-        regex_filtered_df = self.df[
-            self.df["Topic"].str.contains(pattern, case=False, na=False, regex=True)
-        ]
-
-        self.df = pd.concat([regex_filtered_df]).reset_index(drop=True)
-
-    def jobs_description_filter(self):
-        # Remove rows that contain Thai characters
-        self.df = self.df[
-            ~self.df["Qualification"].str.contains(r"[\u0E00-\u0E7F]", na=False)
-        ]
-
-        self.df = self.df.drop_duplicates(subset=["Qualification"]).reset_index(
-            drop=True
-        )
-        return self.df
-
-
 class DataCleaning:
-    def __init__(self, df):
-        self.df = df
+    """Cleaning and filtering"""
+
+    def __init__(self):
         self.html_tags_pattern = re.compile(r"<.*?>")
         self.email_pattern = re.compile(
             r"\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b"
@@ -87,74 +26,108 @@ class DataCleaning:
         self.url_pattern = re.compile(r"http\S+|www\S+")
         self.css_inline_pattern = re.compile(r"\.[a-zA-Z0-9_-]+\s*\{.*?\}", re.DOTALL)
         self.bullet_point_pattern = re.compile(r"[-•*]")
-        self.non_alphanumeric_pattern = re.compile(r"[^a-zA-Z0-9.'\"#\+\-\._ ]+")
+        self.non_alphanumeric_pattern = re.compile(r"[^a-zA-Z0-9.'\"#\+\-\._, ]+")
 
-    def clean(self, text):
-        # Remove CSS styles
+    def filter_non_thai(self, text):
+        """Removes Thai characters"""
+        if isinstance(text, str) and not re.search(r"[\u0E00-\u0E7F]", text):
+            return text
+        return None
+
+    def clean_text(self, text):
+        """Cleans text"""
+        if not isinstance(text, str):
+            return ""
         text = self.css_pattern.sub("", text)
         text = self.css_inline_pattern.sub("", text)
-        # Remove HTML tags
         text = self.html_tags_pattern.sub("", text)
-        # Remove emails
         text = self.email_pattern.sub("", text)
-        # Remove phone numbers
         text = self.phone_pattern.sub("", text)
-        # Remove URLs
         text = self.url_pattern.sub("", text)
-        # Remove bullet points
         text = self.bullet_point_pattern.sub("", text)
-        # Remove non-alphanumeric characters
         text = self.non_alphanumeric_pattern.sub(" ", text)
-        # Remove extra whitespace
         text = re.sub(r"\s+", " ", text).strip()
         return text
 
 
 class DataSegmentation:
+    """Segments text data into sentences using Spacy."""
+
     def __init__(self):
         self.nlp = spacy.load("en_core_web_sm")
 
     def split_sentences(self, text):
+        """Splits text into sentences using Spacy."""
         doc = self.nlp(text)
         return [sent.text.strip() for sent in doc.sents]
 
 
+class DataQualityCheck:
+    """Quality checks on dataset."""
+
+    def __init__(self, df):
+        self.df = df
+
+    def check_missing_values(self):
+        """Checks for missing values"""
+        missing_values = self.df.isnull().sum()
+        print("\n Missing Values:")
+        print(missing_values)
+
+    def check_duplicates(self):
+        """Checks for duplicate values"""
+        duplicated_values = self.df.duplicated().sum()
+        print("\n Duplicated Values in: ", duplicated_values)
+
+    def run_checks(self):
+        """Runs data quality checks."""
+        print("\n Running Data Quality Checks...")
+        self.check_missing_values()
+        self.check_duplicates()
+
+
+class JobDataProcessor:
+    """Main class"""
+
+    def __init__(self, raw_data_path, interim_data_path):
+        self.raw_data_path = raw_data_path
+        self.interim_data_path = interim_data_path
+        self.cleaner = DataCleaning()
+        self.segmenter = DataSegmentation()
+
+    def process(self):
+        """segmenting, and checking quality."""
+        df = pd.read_csv(self.raw_data_path / "merged.csv")
+
+        # Drop duplicate qualifications
+        df.drop_duplicates(subset=["Qualification"], inplace=True)
+
+        # Filter non-Thai topics and qualifications
+        df["Topic"] = df["Topic"].progress_apply(self.cleaner.filter_non_thai)
+        df["Qualification"] = df["Qualification"].progress_apply(
+            self.cleaner.filter_non_thai
+        )
+        df.dropna(subset=["Topic", "Qualification"], inplace=True)
+
+        # Clean qualification text
+        df["Qualification"] = df["Qualification"].apply(self.cleaner.clean_text)
+
+        # Segment qualification text into sentences
+        df["Sentence_Index"] = df.index
+        df["Segmented_Qualification"] = df["Qualification"].progress_apply(
+            self.segmenter.split_sentences
+        )
+        df = df.explode("Segmented_Qualification").reset_index(drop=True)
+        df = df[["Topic", "Sentence_Index", "Segmented_Qualification"]]
+
+        # Save processed data
+        df.to_csv(self.interim_data_path / "segmented_data.csv", index=False)
+
+        # Run data quality checks
+        data_quality = DataQualityCheck(df)
+        data_quality.run_checks()
+
+
 if __name__ == "__main__":
-    raw_data_path = RAW_DATA_PATH
-    interim_data_path = INTERIM_DATA_PATH
-    keywords_data_path = KEYWORDS_DATA_PATH
-
-    # Data Selection
-    data = DataSelection(raw_data_path, keywords_data_path)
-    data.clean_unused_columns()
-    data.jobs_title_filter()
-    selectiondata = data.jobs_description_filter()
-
-    # Data Cleaning
-    cleaningdata = selectiondata
-    cleaningdata["Qualification"] = cleaningdata["Qualification"].apply(
-        DataCleaning(cleaningdata).clean
-    )
-
-    print(cleaningdata)
-
-    # Data Segmentation
-    segmenter = DataSegmentation()
-    segmentationdata = cleaningdata.copy()
-    segmentationdata["Sentence_Index"] = segmentationdata.index
-
-    segmentationdata["Segmented_Qualification"] = segmentationdata[
-        "Qualification"
-    ].progress_apply(segmenter.split_sentences)
-
-    segmentationdata = segmentationdata.explode("Segmented_Qualification").reset_index(
-        drop=True
-    )
-
-    segmentationdata = segmentationdata[
-        ["Topic", "Sentence_Index", "Segmented_Qualification"]
-    ]
-
-    segmentationdata.to_csv(interim_data_path / "segmented_data.csv", index=False)
-
-    print(segmentationdata)
+    processor = JobDataProcessor(RAW_DATA_PATH, INTERIM_DATA_PATH)
+    processor.process()
